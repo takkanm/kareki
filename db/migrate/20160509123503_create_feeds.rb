128d6548b270c9d2c267c41609dc50677267f7d2
Mitsutaka Mimura <takkanm@gmail.com>
2016-05-10 08:38:50 +0900

    fix column type

--- db/migrate/20160509123503_create_feeds.rb
+++ db/migrate/20160509123503_create_feeds.rb
@@ -2,7 +2,7 @@ class CreateFeeds < ActiveRecord::Migration[5.0]
   def change
     create_table :feeds do |t|
       t.string :url
-      t.time :last_updated_at
+      t.datetime :last_updated_at
       t.string :title
 
       t.timestamps
